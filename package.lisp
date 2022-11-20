972cb0164679b0b6c3586bc32b846ad829f43076
Mark Watson <markw@markwatson.com>
2022-11-19 21:05:06 -0700

    added missing dependency on my KBNLP package

--- package.lisp
+++ package.lisp
@@ -2,5 +2,5 @@
 
 (defpackage #:kgn-capi-ui
   (:use #:cl)
-  (:use #:kgn-common #:sparql #:lw-grapher #:trivial-open-browser)
+  (:use #:kgn-common #:sparql #:lw-grapher #:kbnlp #:trivial-open-browser)
   (:export #:kgn-capi-ui))
